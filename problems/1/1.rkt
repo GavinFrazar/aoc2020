97920b48c15b34555a9c6d8dfd825301f390e815
GAVIN FRAZAR <gf5822@att.com>
2020-12-09 10:52:14 -0800

    Refactor to be more idiomatic

--- problems/1/1.rkt
+++ problems/1/1.rkt
@@ -1,21 +1,33 @@
 #lang racket
 
-(define expenses (with-input-from-file "input.txt"
+;; helpers
+(define (inc x)
+  (+ x 1))
+(define (dec x)
+  (- x 1))
+(define nil '())
+
+;; read expenses
+(define expenses
+  (with-input-from-file "input.txt"
     (lambda ()
-      (vector-sort (for/vector ([l (in-lines)])
-              (string->number l))
-            <))))
+      (vector-sort
+       (for/vector ([l (in-lines)])
+         (string->number l))
+       <))))
 
+;; find the two-sum pair
 (define (find-two-sum goal nums)
-  (define (iter left-idx right-idx)
-    (let ((sum-of-pair (+ (vector-ref nums left-idx)
-                          (vector-ref nums right-idx))))
-      (cond ([>= left-idx right-idx] '())
-            ([> sum-of-pair goal] (iter left-idx (- right-idx 1)))
-            ([< sum-of-pair goal] (iter (+ left-idx 1) right-idx))
-            (else (cons (vector-ref nums left-idx)
-                        (vector-ref nums right-idx))))))
-  (iter 0 (- (vector-length nums) 1)))
+  (let iter-find-two-sum ([left-idx 0] [right-idx (- (vector-length nums) 1)])
+    (if (>= left-idx right-idx)
+        nil
+        (let* ([left-val (vector-ref nums left-idx)]
+               [right-val (vector-ref nums right-idx)]
+               [sum-of-pair (+ left-val right-val)])
+          (cond ([> sum-of-pair goal] (iter-find-two-sum left-idx (dec right-idx)))
+                ([< sum-of-pair goal] (iter-find-two-sum (inc left-idx) right-idx))
+                (else (cons left-val right-val)))))))
 
+;; solve
 (let ((two-sum (find-two-sum 2020 expenses)))
-      (* (car two-sum) (cdr two-sum)))
+  (* (car two-sum) (cdr two-sum)))
